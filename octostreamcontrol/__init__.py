e46808499cd43934041bd64f7fc1e234ed1b21a4
Carl Svensson <csvenss2@gmail.com>
2025-07-05 20:20:30 -0700

    success?

--- octostreamcontrol/__init__.py
+++ octostreamcontrol/__init__.py
@@ -1,63 +1,94 @@
-# coding=utf-8
-from __future__ import absolute_import
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
+# octostreamcontrol/__init__.py
+import os, subprocess
 from octoprint.plugin import (
-    StartupPlugin, SettingsPlugin, EventHandlerPlugin, TemplatePlugin
+  StartupPlugin, SettingsPlugin, TemplatePlugin,
+  AssetPlugin, EventHandlerPlugin, SimpleApiPlugin
 )
-import subprocess
 
-class OctoStreamControlPlugin(StartupPlugin, SettingsPlugin, EventHandlerPlugin, TemplatePlugin):
+class OctoStreamControlPlugin(
+    StartupPlugin,
+    SettingsPlugin,
+    TemplatePlugin,
+    AssetPlugin,
+    EventHandlerPlugin,
+    SimpleApiPlugin
+):
 
-    def on_after_startup(self):
-        self._logger.info("OctoStreamControl loaded and ready.")
+  ##--- Plugin metadata (optional, but helps) ---##
+  def get_update_information(self):
+    return {
+      "octostreamcontrol": {
+        "displayName": "OctoStream Control",
+        "displayVersion": self._plugin_version,
 
-    def on_event(self, event, payload):
-        if event == "PrintStarted":
-            self.start_recording()
-        elif event in ("PrintCancelled", "PrintFailed", "PrintDone"):
-            self.stop_recording()
+        # version check URL etc...
+        "type": "github_release",
+        "user": "you",
+        "repo": "octostreamcontrol",
+        "current": self._plugin_version,
 
-    def start_recording(self):
-        cmd = [
-            "/usr/bin/ffmpeg",
-            "-y",
-            "-i", self._settings.get(["stream_url"]),
-            "-c:v", "copy",
-            f"/home/pi/videos/print_{self._printer.get_current_job()['file']['name']}.mp4"
-        ]
-        self._recording_process = subprocess.Popen(cmd)
-        self._logger.info("Recording started.")
+        "pip": "https://github.com/you/octostreamcontrol/archive/{target_version}.zip"
+      }
+    }
 
-    def stop_recording(self):
-        if hasattr(self, "_recording_process") and self._recording_process.poll() is None:
-            self._recording_process.terminate()
-            self._logger.info("Recording stopped.")
+  ##--- SettingsPlugin ---##
+  def get_settings_defaults(self):
+    return {
+      "stream_url": "http://127.0.0.1:8889/webrtc/mystream",
+      "resolution": "640x360"
+    }
 
-    def get_settings_defaults(self):
-        return {
-            "stream_url": "rtsp://localhost:8554/mystream",
-            "resolution": "640x360"
-        }
+  ##--- TemplatePlugin ---##
+  def get_template_configs(self):
+    return [
+      {
+        "type": "settings",
+        "custom_bindings": False
+      },
+      {
+        "type": "tab",
+        "template": "octostreamcontrol_tab.jinja2",
+        "custom_bindings": True
+      }
+    ]
 
-    def get_template_configs(self):
-        return [
-            dict(type="settings", custom_bindings=False),
-            dict(type="tab", name="Live Stream")
-        ]
+  ##--- AssetPlugin ---##
+  def get_assets(self):
+    return {
+      "js": ["js/octostreamcontrol.js"],
+      "css": ["css/octostreamcontrol.css"]
+    }
 
-    def get_api_commands(self):
-        return dict(start=[], stop=[])
+  ##--- EventHandlerPlugin ---##
+  def on_event(self, event, payload):
+    if event == "PrintStarted":
+      self.start_recording()
+    elif event in ("PrintDone", "PrintFailed", "PrintCancelled"):
+      self.stop_recording()
 
-    def on_api_command(self, command, data):
-        if command == "start":
-            self.start_recording()
-        elif command == "stop":
-            self.stop_recording()
+  ##--- SimpleApiPlugin ---##
+  def get_api_commands(self):
+    return {"start": [], "stop": []}
+
+  def on_api_command(self, command, data):
+    if command == "start":
+      self.start_recording()
+    elif command == "stop":
+      self.stop_recording()
+
+  ##--- Recording logic ---##
+  def start_recording(self):
+    url   = self._settings.get(["stream_url"])
+    fname = f"/home/pi/videos/{self._printer.get_current_job()['file']['name']}.mp4"
+    cmd   = ["ffmpeg", "-y", "-i", url, "-c:v", "copy", fname]
+    self._rec = subprocess.Popen(cmd)
+    self._logger.info(f"Started recording to {fname}")
+
+  def stop_recording(self):
+    if hasattr(self, "_rec") and self._rec.poll() is None:
+      self._rec.terminate()
+      self._logger.info("Stopped recording")
+
+__plugin_name__ = "OctoStream Control"
+__plugin_pythoncompat__ = ">=3,<4"
+__plugin_implementation__ = OctoStreamControlPlugin()
